d0287b132f8f36cfb54d4e180dafe55f1409230c
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-03-27 21:45:53 -0300

    feat: start project Calculadora

---
+++ src/main/java/gqs/App.java
@@ -0,0 +1,13 @@
+package gqs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/gqs/MatematicaTest.java
@@ -0,0 +1,55 @@
+package gqs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MatematicaTest {
+    @Test
+    public void deveSomar(){
+        // 3A
+        // Arrange - Cenário
+        Matematica matematica = 
+            new Matematica();
+
+        // Action - Ação 
+        int resultado = 
+            matematica.somar(8, 5);            
+
+        // Assert - Asserção
+        assertEquals(13, resultado);
+    }
+
+    @Test
+    public void deveSubtrair(){
+        // 3A
+        // Arrange - Cenário
+        Matematica matematica = 
+            new Matematica();
+
+        // Action - Ação 
+        int resultado = 
+            matematica.subtrair(8, 5);            
+
+        // Assert - Asserção
+        assertEquals(3, resultado);
+    }
+
+    @Test
+    public void devMultiplicar(){
+        // 3A
+        // Arrange - Cenário
+        Matematica matematica = 
+            new Matematica();
+
+        // Action - Ação 
+        int resultado = 
+            matematica.subtrair(8, 5);            
+
+        // Assert - Asserção
+        assertEquals(40, resultado);
+    }
+
+
+    
+}
