951b92eb2fb880a8a67174381c3b46fc35837e6d
Giovane Barcelos <giovanebarcelos@gmail.com>
2024-04-03 19:38:17 -0300

    feat: subração de 2 números

--- src/main/java/gqs/Matematica.java
+++ src/main/java/gqs/Matematica.java
@@ -6,7 +6,7 @@ public class Matematica {
     }
 
     public int subtrair(int num1, int num2){
-        return 0;
+        return num1 - num2;
     }
 
     public int multiplicar(int num1, int num2){
